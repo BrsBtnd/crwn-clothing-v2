608847d3c6f2e275bc13ec69749c1049686cfdd0
Botond Boros <botond.boros@fortech.ro>
2022-03-31 19:51:02 +0300

    terminate redux updates

--- src/store/Store.js
+++ src/store/Store.js
@@ -1,10 +1,31 @@
 import { compose, createStore, applyMiddleware } from 'redux';
-import logger from "redux-logger";
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import thunk from 'redux-thunk';
+
+import logger from 'redux-logger';
 
 import { rootReducer } from './RootReducer';
 
-const middleWares = [logger];
+const middleWares = [
+  process.env.NODE_ENV === 'development' && logger,
+  thunk,
+].filter(Boolean);
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+const persistConfig = {
+  key: 'root',
+  storage,
+  blacklist: ['user'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = createStore(
+  persistedReducer,
+  undefined,
+  composedEnhancers
+);
+
+export const persistor = persistStore(store);
